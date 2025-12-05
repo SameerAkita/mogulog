13626b9abcbb816835b21a8de62f4e7b2445a0c7
Sameer <akita.sameer1@gmail.com>
2025-12-06 00:04:47 +0900

    widget_tree body valueListenableBuilder - navigation works

--- lib/views/widget_tree.dart
+++ lib/views/widget_tree.dart
@@ -1,17 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:mogulog/data/notifiers.dart';
 import 'package:mogulog/views/pages/analytics_page.dart';
 import 'package:mogulog/views/pages/home_page.dart';
 import 'package:mogulog/views/pages/settings_page.dart';
 import 'package:mogulog/views/pages/upload_page.dart';
 import 'package:mogulog/widgets/navbar_widget.dart';
 
-List<Widget> pages = [HomePage(), AnalyticsPage(), SettingsPage(), UploadPage()];
+List<Widget> pages = [
+  HomePage(),
+  AnalyticsPage(),
+  SettingsPage(),
+  UploadPage(),
+];
 
 class WidgetTree extends StatelessWidget {
   const WidgetTree({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: AnalyticsPage(), bottomNavigationBar: NavbarWidget());
+    return Scaffold(
+      body: ValueListenableBuilder(
+        valueListenable: selectedPageNotifier,
+        builder: (builder, selectedPage, child) {
+          return pages.elementAt(selectedPage);
+        },
+      ),
+      bottomNavigationBar: NavbarWidget(),
+    );
   }
 }
