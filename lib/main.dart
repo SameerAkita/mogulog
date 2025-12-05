ae2f3e1e2f6d1378c9a808beeead62141849a4ce
Sameer <akita.sameer1@gmail.com>
2025-12-05 13:34:17 +0900

    home_page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mogulog/views/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.white),
       ),
-      home: Scaffold(body: Center(child: Text('mogulog'))),
+      home: HomePage(),
     );
   }
 }
