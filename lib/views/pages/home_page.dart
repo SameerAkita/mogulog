48b398408f99b17ea59542a9cb0f96d9543a57ca
Sameer <akita.sameer1@gmail.com>
2025-12-05 14:34:32 +0900

    home page calories left

--- lib/views/pages/home_page.dart
+++ lib/views/pages/home_page.dart
@@ -5,6 +5,69 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('mogulog')));
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: FractionallySizedBox(
+            widthFactor: 0.9,
+            child: Column(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'mogulog',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 40,
+                        ),
+                      ),
+                      Icon(Icons.notifications_none_rounded, size: 40),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Card(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              '1250',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 40,
+                              ),
+                            ),
+                            Text(
+                              'calories left',
+                              style: TextStyle(fontSize: 16),
+                            ),
+                          ],
+                        ),
+                        Text(
+                          'O',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 100,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(flex: 2, child: Container(color: Colors.green)),
+                Expanded(flex: 4, child: Container(color: Colors.yellow)),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
