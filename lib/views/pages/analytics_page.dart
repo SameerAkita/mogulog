c95dbbdf7511d0ab7b302fab32eda99c8a2181c4
Sameer <akita.sameer1@gmail.com>
2026-01-10 00:10:02 +0900

    analytics to stateful widget

--- lib/views/pages/analytics_page.dart
+++ lib/views/pages/analytics_page.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:mogulog/theme/app_colors.dart';
 
-class AnalyticsPage extends StatelessWidget {
+class AnalyticsPage extends StatefulWidget {
   const AnalyticsPage({super.key});
 
+  @override
+  State<AnalyticsPage> createState() => _AnalyticsPageState();
+}
+
+class _AnalyticsPageState extends State<AnalyticsPage> {
+  Color color = Colors.blue;
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -24,7 +30,43 @@ class AnalyticsPage extends StatelessWidget {
                       padding: const EdgeInsets.all(8.0),
                       child: Column(
                         children: [
-                          Row(children: [Text('data')]),
+                          Container(
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(8),
+                            ),
+                            padding: EdgeInsets.symmetric(vertical: 2),
+
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                InkWell(
+                                  child: Container(
+                                    color: color,
+                                    child: Text(
+                                      '1 Week',
+                                      style: TextStyle(fontSize: 18),
+                                    ),
+                                  ),
+                                  onTap: () {
+                                    setState(() {
+                                      color = Colors.red;
+                                    });
+                                  },
+                                ),
+                                Text('1 Month', style: TextStyle(fontSize: 18)),
+                                Text(
+                                  '6 Months',
+                                  style: TextStyle(fontSize: 18),
+                                ),
+                                Text(
+                                  'All time',
+                                  style: TextStyle(fontSize: 18),
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(height: 8),
                           Expanded(
                             child: Container(
                               decoration: BoxDecoration(
