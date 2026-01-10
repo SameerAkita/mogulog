427a9b478533298870430249162e9e0d06065bef
Sameer <akita.sameer1@gmail.com>
2026-01-10 20:19:57 +0900

    calories widget circular progress indicator

--- lib/widgets/analytics/calories_widget.dart
+++ lib/widgets/analytics/calories_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mogulog/theme/app_colors.dart';
 
 class CaloriesWidget extends StatelessWidget {
   const CaloriesWidget({super.key});
@@ -18,9 +19,32 @@ class CaloriesWidget extends StatelessWidget {
             children: [
               Text(
                 'Calories',
-                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
               ),
-              Expanded(child: Container(color: Colors.red)),
+              Expanded(
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        left: 6,
+                      ), // padding counteracts the circularprogressindicator's stroke width
+                      child: SizedBox(
+                        height: 110.0,
+                        width: 110.0,
+                        child: CircularProgressIndicator(
+                          value: 0.8,
+                          valueColor: AlwaysStoppedAnimation<Color>(
+                            Colors.black,
+                          ),
+                          backgroundColor: AppColors.lightgrey,
+                          strokeWidth: 10,
+                          strokeCap: StrokeCap.round,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
--- lib/widgets/analytics/weight_widget.dart
+++ lib/widgets/analytics/weight_widget.dart
@@ -6,28 +6,25 @@ class WeightWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Weight',
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                        Expanded(child: Container(color: Colors.green)),
-                      ],
-                    ),
-                  ),
-                ),
-              );
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Weight',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+              ),
+              Expanded(child: Container(color: Colors.green)),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
