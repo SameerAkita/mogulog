de510f710051b275ed55a82a2407c01d778ba588
Sameer <akita.sameer1@gmail.com>
2026-01-21 23:39:23 +0900

    add styling

--- lib/widgets/settings/settings_widget.dart
+++ lib/widgets/settings/settings_widget.dart
@@ -5,6 +5,18 @@ class SettingsWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.red, child: Text('settings option'));
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.black, width: 1),
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: Padding(
+        padding: EdgeInsetsGeometry.symmetric(vertical: 4, horizontal: 12),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [Text('settings option'), Icon(Icons.arrow_forward_ios)],
+        ),
+      ),
+    );
   }
 }
