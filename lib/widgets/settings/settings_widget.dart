58fc498e8ab415fed70cbc8cdc9c403eb137d77c
Sameer <akita.sameer1@gmail.com>
2026-01-23 18:17:09 +0900

    refactor to material widget

--- lib/widgets/settings/settings_widget.dart
+++ lib/widgets/settings/settings_widget.dart
@@ -7,22 +7,22 @@ class SettingsWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
+    return Material(
+      color: Colors.white,
       borderRadius: BorderRadius.circular(15),
+      child: InkWell(
+        onTap: () {},
+        borderRadius: BorderRadius.circular(15),
 
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(8),
-        ),
-        margin: EdgeInsets.only(top: 8),
-        padding: EdgeInsetsGeometry.symmetric(vertical: 12, horizontal: 16),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(title, style: TextStyle(fontSize: 16)),
-            Icon(Icons.arrow_forward_ios, size: 16),
-          ],
+        child: Padding(
+          padding: EdgeInsetsGeometry.symmetric(vertical: 12, horizontal: 16),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(title, style: TextStyle(fontSize: 16)),
+              Icon(Icons.arrow_forward_ios, size: 16),
+            ],
+          ),
         ),
       ),
     );
