fd8a0b6f1fd0fd8384a7526e372bec2b8d535d5f
Sameer <akita.sameer1@gmail.com>
2026-01-23 20:25:38 +0900

    add pagecontroller

--- lib/features/onboarding/onboarding_page.dart
+++ lib/features/onboarding/onboarding_page.dart
@@ -1,14 +1,44 @@
 import 'package:flutter/material.dart';
 
-class OnboardingPage extends StatelessWidget {
+class OnboardingPage extends StatefulWidget {
   const OnboardingPage({super.key});
+
+  @override
+  State<OnboardingPage> createState() => _OnboardingPageState();
+}
+
+class _OnboardingPageState extends State<OnboardingPage> {
+  final PageController _pageController = PageController();
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: PageView(
+        controller: _pageController,
+        physics: const NeverScrollableScrollPhysics(),
         children: [
-          Container(color: Colors.red),
+          Container(
+            color: Colors.red,
+            child: Center(
+              child: ElevatedButton(
+                onPressed: () {
+                  if (_pageController.hasClients) {
+                    _pageController.nextPage(
+                      duration: const Duration(milliseconds: 400),
+                      curve: Curves.easeInOut,
+                    );
+                  }
+                },
+                child: const Text('Next'),
+              ),
+            ),
+          ),
           Container(color: Colors.blue),
           Container(color: Colors.green),
         ],
---
+++ lib/features/onboarding/onboarding_step_welcome.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OnboardingStepWelcome extends StatelessWidget {
+  const OnboardingStepWelcome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(color: Colors.red);
+  }
+}
