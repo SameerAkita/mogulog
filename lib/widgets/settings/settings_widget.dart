3dac19d9162c0e57b5976d35fe92606da39887a9
Sameer <akita.sameer1@gmail.com>
2026-01-23 18:09:57 +0900

    settings widget styling

--- lib/widgets/settings/settings_widget.dart
+++ lib/widgets/settings/settings_widget.dart
@@ -9,12 +9,20 @@ class SettingsWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return InkWell(
       borderRadius: BorderRadius.circular(15),
-      
-      child: Padding(
-        padding: EdgeInsetsGeometry.symmetric(vertical: 4, horizontal: 12),
+
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        margin: EdgeInsets.only(top: 8),
+        padding: EdgeInsetsGeometry.symmetric(vertical: 12, horizontal: 16),
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [Text(title), Icon(Icons.arrow_forward_ios)],
+          children: [
+            Text(title, style: TextStyle(fontSize: 16)),
+            Icon(Icons.arrow_forward_ios, size: 16),
+          ],
         ),
       ),
     );
